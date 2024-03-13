d2dd4a5afafd773ce36801db92124c121eb53d54
SZandi0022 <162721033+SZandi0022@users.noreply.github.com>
2024-03-13 04:00:16 +0330

    Add files via upload

---
+++ login.cy.js
@@ -0,0 +1,52 @@
+/// <refrence types = *cypress* />
+
+describe('test suit login',function(){
+    before(function (){
+    it ('doshboard test', ()=>{
+        cy.visit("./project/login/login.html")
+        
+    
+        cy.get('#login-email').type("saeedzandi29@gmail.com",{delay:100})
+        cy.get('#login-password').type("abc123456789",{delay:100})
+      cy.log('test stup user')
+    })
+    })
+    after(function(){ 
+        cy.log('Test Terddown');
+    })
+})
+   //test keyword 
+    describe('test keyword login',function(){
+        
+        it ('doshboard test', ()=>{
+            cy.visit("./project/login/login.html")
+        
+            cy.get('#login-email').type("saedzandi29@gmail.com",{delay:50}).clear().type("saedzandi29@gamil.com ")
+            cy.get('#login-password').type("abc123456789",{delay:50}).type('{backspace}{home}{selectall}{del}')
+            .type("Abc123456789")
+            cy.get('#login-password').type("{enter}")
+        })
+        })
+        describe('test muose action',function(){
+            it ('mouse action', ()=>{
+                cy.visit("./project/login/login.html")
+            
+                cy.get('#login-email').type("saeedzandi29@gmail.com",{delay:50}).dblclick().should('have.value','saeedzandi29@gmail.com')
+                cy.get('#login-password').type("abc123456789",{delay:50})
+
+                cy.contains("Log In").click()
+                cy.get('a').click()
+               
+            })
+            })
+        
+        
+           //* it('test key holde', ()=>{
+               //* cy.visit("./project/login/login.html")
+            
+                //* cy.get('#login-email').type("saeedzandi29@gmail.com")
+                //* .realpress(['control ',' A'])
+                //* .realpress(['control ',' X'])
+               //  .realpress(['control ','V'])
+               //* cy.get('#login-password').type("abc123456789").click
+            //* })
